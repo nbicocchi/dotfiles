282edb8e856f363b3d0c3f5fc14acd19f04db285
Nicola Bicocchi <nbicocchi@unimore.it>
2023-10-02 00:25:09 +0200

    update

--- home/install_scripts/executable_install_script.sh
+++ home/install_scripts/executable_install_script.sh
@@ -37,8 +37,7 @@ install_aur_pkgs() {
 install_flatpak_pkgs() {
   pkgs="com.google.Chrome com.google.EarthPro org.nickvision.cavalier
     de.haeckerfelix.Shortwave com.valvesoftware.Steam io.missioncenter.MissionCenter 
-    com.visualstudio.code us.zoom.Zoom io.github.mimbrero.WhatsAppDesktop
-    com.discordapp.Discord"
+    us.zoom.Zoom io.github.mimbrero.WhatsAppDesktop com.discordapp.Discord"
   for pkg in $pkgs; do
     echo "Installing" "$pkg"...
     flatpak install --system --noninteractive --assumeyes flathub "$pkg" 
