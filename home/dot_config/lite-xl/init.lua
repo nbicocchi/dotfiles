8289464c1d0c5da373365f338f6e975527bc1988
Nicola Bicocchi <nbicocchi@unimore.it>
2024-07-05 09:21:04 +0200

    update

---
+++ home/dot_config/lite-xl/init.lua
@@ -0,0 +1,98 @@
+-- put user settings here
+-- this module will be loaded after everything else when the application starts
+-- it will be automatically reloaded when saved
+
+local core = require "core"
+local keymap = require "core.keymap"
+local config = require "core.config"
+local style = require "core.style"
+local common = require "core.common"
+
+------------------------------ Themes ----------------------------------------
+
+-- light theme:
+core.reload_module("colors.winter")
+
+--------------------------- Key bindings -------------------------------------
+
+-- key binding:
+-- keymap.add { ["ctrl+escape"] = "core:quit" }
+
+-- pass 'true' for second parameter to overwrite an existing binding
+-- keymap.add({ ["ctrl+pageup"] = "root:switch-to-previous-tab" }, true)
+-- keymap.add({ ["ctrl+pagedown"] = "root:switch-to-next-tab" }, true)
+
+------------------------------- Fonts ----------------------------------------
+
+-- customize fonts:
+style.font = renderer.font.load(DATADIR .. "/fonts/FiraSans-Regular.ttf", 30 * SCALE)
+style.code_font = renderer.font.load(DATADIR .. "/fonts/JetBrainsMono-Regular.ttf", 30 * SCALE)
+style.icon_font = renderer.font.load(DATADIR .. "/fonts/icons.ttf", 24 * SCALE)
+--
+-- DATADIR is the location of the installed Lite XL Lua code, default color
+-- schemes and fonts.
+-- USERDIR is the location of the Lite XL configuration directory.
+--
+-- font names used by lite:
+-- style.font          : user interface
+-- style.big_font      : big text in welcome screen
+-- style.icon_font     : icons
+-- style.icon_big_font : toolbar icons
+-- style.code_font     : code
+--
+-- the function to load the font accept a 3rd optional argument like:
+--
+-- {antialiasing="grayscale", hinting="full", bold=true, italic=true, underline=true, smoothing=true, strikethrough=true}
+--
+-- possible values are:
+-- antialiasing: grayscale, subpixel
+-- hinting: none, slight, full
+-- bold: true, false
+-- italic: true, false
+-- underline: true, false
+-- smoothing: true, false
+-- strikethrough: true, false
+
+------------------------------ Plugins ----------------------------------------
+
+-- disable plugin loading setting config entries:
+
+-- disable plugin detectindent, otherwise it is enabled by default:
+-- config.plugins.detectindent = false
+
+---------------------------- Miscellaneous -------------------------------------
+
+-- modify list of files to ignore when indexing the project:
+-- config.ignore_files = {
+--   -- folders
+--   "^%.svn/",        "^%.git/",   "^%.hg/",        "^CVS/", "^%.Trash/", "^%.Trash%-.*/",
+--   "^node_modules/", "^%.cache/", "^__pycache__/",
+--   -- files
+--   "%.pyc$",         "%.pyo$",       "%.exe$",        "%.dll$",   "%.obj$", "%.o$",
+--   "%.a$",           "%.lib$",       "%.so$",         "%.dylib$", "%.ncb$", "%.sdf$",
+--   "%.suo$",         "%.pdb$",       "%.idb$",        "%.class$", "%.psd$", "%.db$",
+--   "^desktop%.ini$", "^%.DS_Store$", "^%.directory$",
+-- }
+
+config.tab_type = "soft" -- soft for spaces, hard for real tabs (\t)
+config.indent_size = 4   -- 4 spaces
+config.borderless = true
+
+config.plugins.treeview = common.merge({
+  -- Default treeview width
+  size = 400 * SCALE
+}, config.plugins.treeview)
+
+config.plugins.terminal = common.merge({
+  -- the default height of the console drawer
+  drawer_height = 600,
+}, config.plugins.terminal)
+
+config.plugins.spellcheck = common.merge({
+  enabled = true,
+  files = { "%.txt$", "%.md$", "%.markdown$" },
+  -- dictionary_file = "/usr/share/hunspell/it_IT.dic"
+  dictionary_file = "/usr/share/hunspell/en_US.dic"
+}, config.plugins.spellcheck)
+
+
