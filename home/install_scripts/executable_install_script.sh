3d0439086681d821a3754f722e805c6d3acff542
Nicola Bicocchi <nbicocchi@unimore.it>
2024-07-09 01:50:24 +0200

    update

--- home/install_scripts/executable_install_script.sh
+++ home/install_scripts/executable_install_script.sh
@@ -17,11 +17,11 @@ install_base_cli_pkgs() {
 }
 
 install_base_gui_pkgs() {
-  pkgs="vlc font-manager thunderbird inkscape transmission-gtk lollypop filezilla helvum
+  pkgs="vlc font-manager thunderbird inkscape lite-xl lollypop filezilla helvum
     foliate handbrake xournalpp signal-desktop nerd-fonts libreoffice-fresh discord gimp 
     conky inkscape cheese gthumb eog evince seahorse gvfs-goa gvfs-google gnome-screenshot
     gnome-sound-recorder gnome-shell-extension-dash-to-panel gnome-shell-extension-desktop-icons-ng
-    lite-xl"
+    fragments"
   sudo pacman -S --noconfirm --needed $pkgs
 }
 
