53ffcb656721d005f6dd73a6adc9bbf9e0cd8eaa
Nicola Bicocchi <nbicocchi@unimore.it>
2024-08-06 23:44:43 +0200

    update

--- home/install_scripts/executable_install_script.sh
+++ home/install_scripts/executable_install_script.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
 install_base_cli_pkgs() {
-  pkgs="zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
-    zsh-completions starship 
+  pkgs="zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-completions starship 
     pacman-contrib base-devel gdb git gnupg openssh net-tools curl wget mc micro vsftpd 
     btop chezmoi man-db bat bat-extras dua-cli trash-cli lazygit ripgrep fzf
     neovim neofetch xclip xsel fd eza shellcheck snapper zoxide exfatprogs ctags 
-    hunspell hunspell-it hunspell-en_us pyenv
-    maven gradle jdk17-openjdk jdk21-openjdk jenv"
+    hunspell hunspell-it hunspell-en_us 
+    maven gradle jdk17-openjdk jdk21-openjdk jenv
+    pyenv"
   sudo pacman -S --noconfirm --needed $pkgs
   chsh -s /bin/zsh
 }
 
 install_base_gui_pkgs() {
-  pkgs="nerd-fonts font-manager chromium firefox thunderbird inkscape lite-xl lollypop filezilla
+  pkgs="nerd-fonts font-manager firefox thunderbird lite-xl lollypop filezilla
     helvum foliate handbrake xournalpp signal-desktop libreoffice-fresh discord gimp conky vlc 
     cheese gthumb eog evince seahorse gvfs-goa gvfs-google 
     gnome-autoar gnome-backgrounds gnome-bluetooth-3.0 gnome-browser-connector gnome-calculator 
@@ -27,8 +27,8 @@ install_base_gui_pkgs() {
 }
 
 install_aur_pkgs() {
-  pkgs="cava cavasik shortwave chatgpt-desktop-bin chatgpt-shell-cli docker-desktop
-    freetube-bin javafx-scenebuilder popcorntime-bin teams whatsapp-for-linux menulibre adw-gtk3"
+  pkgs="cava cavasik shortwave chatgpt-desktop-bin docker-desktop javafx-scenebuilder 
+  google-chrome visual-studio-code-bin popcorntime-bin menulibre adw-gtk3"
   yay -S --aur --noconfirm $pkgs
 }
 
@@ -41,24 +41,30 @@ install_flatpak_pkgs() {
   done
 }
 
+install_yay() {
+  pkgs="base-devel git"
+  sudo pacman -S --noconfirm --needed $pkgs
+  cd /tmp
+  rm -rf yay
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+}
+
 usage() {
   printf "\nUsage:\n"
   echo " --base-cli Install base cli pkgs"
-  echo " --basi-gui Install base gui pkgs"
+  echo " --base-gui Install base gui pkgs"
   echo " --aur      Install AUR pkgs"
   echo " --flatpak  Install flatpak pkgs"
   exit 1
 }
 
-## CLI options
+## main
 BASECLI=false
 BASEGUI=false
 AUR=false
 FLATPAK=false
-
-if [ "$#" -eq 0 ]; then 
-  usage
-fi
 
 while [ "$#" -gt 0 ] ; do
   case "$1" in
@@ -70,6 +76,13 @@ while [ "$#" -gt 0 ] ; do
   esac
   shift
 done
+
+"$BASECLI" || "$BASEGUI" || "$AUR" || "$FLATPAK" || usage
+
+#sudo pacman -Syyu --noconfirm
+if ! pacman -Qs yay >/dev/null 2>&1; then
+  install_yay
+fi
 
 "$BASECLI" && install_base_cli_pkgs
 "$BASEGUI" && install_base_gui_pkgs
